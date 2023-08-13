a7df6ec15e34a5ad7b35cb3ae72641587d7cb210
inovei6un <92116223+inovei6un@users.noreply.github.com>
2023-08-13 14:41:45 +0300

    Delete lawyer_site_project directory

--- lawyer_site_project/lawyer_site/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from lawyer_site.models import Appointment
-
-
-class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'appointment_datetime', 'phone')
-
-
-admin.site.register(Appointment)
--- lawyer_site_project/lawyer_site/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_letters_only(value):
-    if not value.isalpha():
-        raise ValidationError('Името може да се състои само от букви!')
